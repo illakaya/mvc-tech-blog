8f646ff3d7c7301e80e358f9a0163e1a3407c33e
illakaya <mimijane.nguyen@gmail.com>
2024-06-10 13:37:07 +1000

    trial route for homepage

---
+++ utils/auth.js
@@ -0,0 +1,11 @@
+const withAuth = (req, res, next) => {
+  // If the user is not logged in, redirect the user to the login page
+  // If the user is logged in, allow them to view the posts
+  if (!req.session.loggedIn) {
+    res.redirect('/login');
+  } else {
+    next();
+  }
+};
+
+module.exports = withAuth;
